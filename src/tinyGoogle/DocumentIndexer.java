06e113692cd8e993b406d40fc2728cbd7317dce9
iamaureen <ishrat.ahmed@asu.edu>
2018-11-27 02:07:21 -0500

    implemented document indexer isDocumentPresent(fileid) method

--- src/tinyGoogle/DocumentIndexer.java
+++ src/tinyGoogle/DocumentIndexer.java
@@ -1,6 +1,95 @@
 package tinyGoogle;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class DocumentIndexer {
+	
+	private ArrayList<DocumentIndex> DocIdList = new ArrayList<DocumentIndex>();
+	
+	DocumentIndexer(){
+		parseFile();
+	}
+	
+	void parseFile() {
+		String workingDirectoty = System.getProperty("user.dir");
+		String fileName = "documentList.txt";
+		String filePath = workingDirectoty+"/src/tinyGoogle/" + fileName;
+		
+		System.out.println("Working Directory = " + filePath);
+		
+		BufferedReader br = null;
+		FileReader fr = null;
+
+		try {
+			
+			//br = new BufferedReader(new FileReader(FILENAME));
+			fr = new FileReader(filePath);
+			br = new BufferedReader(fr);
+
+			String sCurrentLine;
+			
+			br.readLine(); //consume the first line and ignore
+
+			while ((sCurrentLine = br.readLine()) != null) {
+				System.out.println(sCurrentLine);
+				String[] sp = sCurrentLine.split(" ");
+				
+				//insert into the list
+				DocumentIndex di = new DocumentIndex();
+				di.setDocId(Integer.parseInt(sp[0]));
+				di.setDocPath(sp[1]);
+				
+				DocIdList.add(di);
+				
+			}
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+			try {
+
+				if (br != null)
+					br.close();
+
+				if (fr != null)
+					fr.close();
+
+			} catch (IOException ex) {
+
+				ex.printStackTrace();
+
+			}
+		}	
+	}
+	
+	public boolean isDocumentPresent(int id) {
+		
+		for(int i = 0; i < DocIdList.size(); i++)
+		{
+		    System.out.println(DocIdList.get(i).getDocId());
+		    if(DocIdList.get(i).getDocId() == id) {
+		    		return true; //return DocIdList.get(i).getDocPath();
+		    }
+		}
+		
+		return false; //file id is not in the list
+		
+	}
+	
+	 public static void main(String[] args) {
+		 //when DocumentIndexer is called it parses through the document and makes a list of existing documents
+		 DocumentIndexer d = new DocumentIndexer();
+		 
+		 System.out.print(d.isDocumentPresent(1));
+	       
+	  }
+
+}
+
+class DocumentIndex{
 	
 	private int DocId;
 	private String DocPath;
@@ -9,7 +98,6 @@ public class DocumentIndexer {
 		return DocId;
 	}
 	public void setDocId(int docId) {
-		
 		DocId = docId;
 	}
 	public String getDocPath() {
@@ -19,8 +107,4 @@ public class DocumentIndexer {
 		DocPath = docPath;
 	}
 	
-	
-	
-	
-
 }
--- src/tinyGoogle/userInterface.java
+++ src/tinyGoogle/userInterface.java
@@ -9,16 +9,18 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.filechooser.FileSystemView;
 
 //https://examples.javacodegeeks.com/desktop-java/swing/java-swing-boxlayout-example/
+//http://www.ntu.edu.sg/home/ehchua/programming/java/j4a_gui.html
+//https://www.geeksforgeeks.org/java-swing-jfilechooser/
 
 public class userInterface extends Frame implements ActionListener {
 	
-	   private Label lblInput;     // Declare input Label
-	   private Label lblOutput;    // Declare output Label
-	   private TextField tfInput;  // Declare input TextField
-	   private TextField tfOutput; // Declare output TextField
-	   private int sum = 0;        // Accumulated sum, init to 0
-	   private javax.swing.JButton jButton_fileChoose;
-	   private static javax.swing.JLabel jLabel_Image;
+	   static Label lblInput;     // Declare input Label
+	   static Label lblOutput;    // Declare output Label
+	   static TextField tfInput;  // Declare input TextField
+	   static TextField tfOutput; // Declare output TextField
+	  
+	   static javax.swing.JButton jButton_fileChoose;
+	   static javax.swing.JLabel jLabel_Image;
 	   
 	   static String pathname;
 	   static String filename;
@@ -27,85 +29,86 @@ public class userInterface extends Frame implements ActionListener {
 	   // Constructor to setup the GUI components and event handlers
 	   public userInterface() {
 		   
-		   // Create and set up a frame window
-			JFrame.setDefaultLookAndFeelDecorated(true);
-			JFrame frame = new JFrame("BoxLayout Example X_AXIS");
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			
-			// Set the panel to add buttons
-			JPanel panel = new JPanel();
-			
-			// Set the BoxLayout to be X_AXIS: from left to right
-			BoxLayout boxlayout = new BoxLayout(panel, BoxLayout.Y_AXIS);
-			
-			// Set the Boxayout to be Y_AXIS from top to down
-			//BoxLayout boxlayout = new BoxLayout(panel, BoxLayout.Y_AXIS);
+		// Create and set up a frame window
+					JFrame.setDefaultLookAndFeelDecorated(true);
+					JFrame frame = new JFrame("BoxLayout Example X_AXIS");
+					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					
+					// Set the panel to add buttons
+					JPanel panel = new JPanel();
+					
+					// Set the BoxLayout to be X_AXIS: from left to right
+					BoxLayout boxlayout = new BoxLayout(panel, BoxLayout.Y_AXIS);
+					
+					// Set the Boxayout to be Y_AXIS from top to down
+					//BoxLayout boxlayout = new BoxLayout(panel, BoxLayout.Y_AXIS);
 
-			panel.setLayout(boxlayout);
+					panel.setLayout(boxlayout);
+					
+					// Set border for the panel
+					panel.setBorder(new EmptyBorder(new Insets(150, 200, 150, 200)));	
+					
+					lblInput = new Label("Option (Index/Query): ");
+					panel.add(lblInput);
+					
+					tfInput = new TextField(10); // Construct TextField
+				    panel.add(tfInput);  
+				    
+				    lblOutput = new Label("Document Path ");
+					panel.add(lblOutput);
+					
+					tfOutput = new TextField(40);
+					panel.add(tfOutput);
+					
+					jButton_fileChoose = new javax.swing.JButton();
+				    panel.add(jButton_fileChoose);
+				      
+				      //file chooser button -- choose text file
+				        jButton_fileChoose.setText("Choose File");
+				        jButton_fileChoose.addActionListener(new java.awt.event.ActionListener() {
+				            public void actionPerformed(java.awt.event.ActionEvent evt) {
+				            	jButtonFileChooseActionPerformed(evt);
+				            }
 			
-			// Set border for the panel
-			panel.setBorder(new EmptyBorder(new Insets(150, 200, 150, 200)));	
-			
-			lblInput = new Label("Option (Index/Query): ");
-			panel.add(lblInput);
-			
-			tfInput = new TextField(10); // Construct TextField
-		    panel.add(tfInput);  
-		    
-		    lblOutput = new Label("Document Path ");
-			panel.add(lblOutput);
-			
-			tfOutput = new TextField(40);
-			panel.add(tfOutput);
-			
-			jButton_fileChoose = new javax.swing.JButton();
-		    panel.add(jButton_fileChoose);
-		      
-		      //file chooser button -- choose text file
-		        jButton_fileChoose.setText("Choose File");
-		        jButton_fileChoose.addActionListener(new java.awt.event.ActionListener() {
-		            public void actionPerformed(java.awt.event.ActionEvent evt) {
-		            	jButtonFileChooseActionPerformed(evt);
-		            }
-	
-					private void jButtonFileChooseActionPerformed(ActionEvent evt) {
-						// TODO Auto-generated method stub
-						
-					JFileChooser fileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory()); 
-				 
-					int result = fileChooser.showOpenDialog(null);
-				    	if (result == JFileChooser.APPROVE_OPTION) {
-				    	    File selectedFile = fileChooser.getSelectedFile();
-				    	    
-				    	    fullPathName=selectedFile.getAbsolutePath();
-				    	    pathname=selectedFile.getParent();
-				    	    filename=selectedFile.getName();
-				    	    
-				    	    System.out.println("Selected file: " + selectedFile.getAbsolutePath()+ "\nfile name: " + selectedFile.getName());
-				    	    tfOutput.setText(selectedFile.getAbsolutePath());
-				    	}
-						
-					}
-		        });
-		        
-				// Define new buttons
-				JButton jb1 = new JButton("Send To Client");
+							private void jButtonFileChooseActionPerformed(ActionEvent evt) {
+								// TODO Auto-generated method stub
+								
+							JFileChooser fileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory()); 
+						 
+							int result = fileChooser.showOpenDialog(null);
+						    	if (result == JFileChooser.APPROVE_OPTION) {
+						    	    File selectedFile = fileChooser.getSelectedFile();
+						    	    
+						    	    fullPathName=selectedFile.getAbsolutePath();
+						    	    pathname=selectedFile.getParent();
+						    	    filename=selectedFile.getName();
+						    	    
+						    	    System.out.println("Selected file: " + selectedFile.getAbsolutePath()+ "\nfile name: " + selectedFile.getName());
+						    	    tfOutput.setText(selectedFile.getAbsolutePath());
+						    	}
+								
+							}
+				        });
+				        
+						// Define new buttons
+						JButton jb1 = new JButton("Send To Client");
 
-				// Add buttons to the frame (and spaces between buttons)
-				panel.add(jb1);
-				
-				jb1.addActionListener(this);
-			
-			// Set size for the frame
-			//frame.setSize(300, 300);
-			
-			// Set the window to be visible as the default to be false
-			frame.add(panel);
-			frame.pack();
-			frame.setVisible(true);
+						// Add buttons to the frame (and spaces between buttons)
+						panel.add(jb1);
+						
+						jb1.addActionListener(this);
+					
+					// Set size for the frame
+					//frame.setSize(300, 300);
+					
+					// Set the window to be visible as the default to be false
+					frame.add(panel);
+					frame.pack();
+					frame.setVisible(true);
 
 	   }
 	   
+	
 	 
 	   // The entry main() method
 	   public static void main(String[] args) {
