bf2971fc2432acf7ba01ceb6bfcbae2d704b40a7
iamaureen <ishrat.ahmed@asu.edu>
2018-11-30 15:09:28 -0500

    file entry if file not present

--- src/tinyGoogle/DocumentIndexer.java
+++ src/tinyGoogle/DocumentIndexer.java
@@ -6,20 +6,19 @@ import java.util.ArrayList;
 public class DocumentIndexer {
 	
 	private ArrayList<DocumentIndex> DocIdList = new ArrayList<DocumentIndex>();
+	int maxID; 
 	
 	DocumentIndexer(){
 		parseFile();
 	}
 	
 	void parseFile() {
-		String workingDirectoty = System.getProperty("user.dir");
-		String fileName = "documentList.txt";
-		String filePath = workingDirectoty+"/src/tinyGoogle/" + fileName;
-		
-		System.out.println("Working Directory = " + filePath);
+		String filePath = getFilePath();
 		
 		BufferedReader br = null;
 		FileReader fr = null;
+		
+		int max = 0;
 
 		try {
 			
@@ -39,11 +38,17 @@ public class DocumentIndexer {
 				DocumentIndex di = new DocumentIndex();
 				di.setDocId(Integer.parseInt(sp[0]));
 				di.setDocPath(sp[1]);
-				
-				
+			
 				DocIdList.add(di);
 				
+				//get the max ID
+				if(Integer.parseInt(sp[0])>max) {
+					max = Integer.parseInt(sp[0]);
+				}
+				
 			}
+			
+			maxID = max;
 
 		} catch (IOException e) {
 
@@ -80,36 +85,68 @@ public class DocumentIndexer {
 		
 	}
 	
-	public boolean isDocumentPresentByPath(String path) {
+	public int isDocumentPresentByPath(String path) {
 		
+		int docID=0;
 		for(int i = 0; i < DocIdList.size(); i++)
 		{
 		    System.out.println(DocIdList.get(i).getDocPath());
 		    if(DocIdList.get(i).getDocPath().trim().equals(path)) {
-		    		return true; //return DocIdList.get(i).getDocPath();
-		    }
+		    		return DocIdList.get(i).getDocId();
+		    } 
 		}
 		
-		return false; //file id is not in the list
+		//enter a new entry (id, path) in the file and then return the ID
+		
+		//increase the maximum id present in the file by 1
+		docID =  maxID+1;
+		String toWriteInTheFile = "\n" + docID + " " + path;
+		//append into the file with id and path
+		appendStrToFile(getFilePath(), toWriteInTheFile);
+		
+		return docID;
+		
+		 //file id is not in the list
 		
 	}
 	
 	
 	
 	public void addFiletoDocumentIndex(String path) {
-		if(!isDocumentPresentByPath(path)) {
-			//add in the document
-			//get the max id, add 1 to it and write to the file
-		}
+		
 		
 		
 	}
+	
+	 public static void appendStrToFile(String fileName, 
+             String str) 
+	{ 
+		try { 
+				// Open given file in append mode. 
+				BufferedWriter out = new BufferedWriter( new FileWriter(fileName, true)); 
+				out.write(str); 
+				out.close(); 
+			} 
+		catch (IOException e) { 
+			System.out.println("exception occoured" + e); 	
+		} 
+	} 
+	 
+	 public String getFilePath() {
+		 	String workingDirectoty = System.getProperty("user.dir");
+			String fileName = "documentList.txt";
+			String filePath = workingDirectoty+"/src/tinyGoogle/" + fileName;
+			
+			System.out.println("Working Directory = " + filePath);
+			
+			return filePath;
+	 }
 	
 	 public static void main(String[] args) {
 		 //when DocumentIndexer is called it parses through the document and makes a list of existing documents
 		 DocumentIndexer d = new DocumentIndexer();
 		 
-		 System.out.print(d.isDocumentPresentByPath("harrypotter"));
+		 System.out.print(d.isDocumentPresentByPath("arrypotter"));
 	       
 	  }
 
